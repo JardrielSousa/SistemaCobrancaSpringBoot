6be0c45ada42e59585937a954a963f00c363ac2f
jardriel sousa <jardriel.redes18@gmail.com>
2021-04-24 23:03:19 -0300

    ajustes front end e editar titulo

---
+++ bin/README.md
@@ -0,0 +1,12 @@
+# Sistema Cobranca Spring Boot
+Sistema simples de cobranca usando spring boot 
+
+## Tecnologias usadas:
+ - Spring Boot 
+ - Api Rest
+ - Java
+ - Html , Javascript e CSS
+ - JPA / Hibernate(Pesistencia)
+ - H2 (Banco de Dados)
+ - Maven
+
--- src/main/java/br/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/br/com/algaworks/cobranca/controller/TituloController.java
@@ -8,8 +8,11 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,6 +23,18 @@ import br.com.algaworks.cobranca.repository.Titulos;
 @Controller
 @RequestMapping(value = "/titulos")
 public class TituloController {
+	private static final String EDITADO_COM_SUCESSO = "Titulo editado com sucesso !";
+
+	private static final String CADASTRO_COM_SUCESSO = "Titulo Cadastrado com sucesso !";
+
+	private static final String MENSAGEM = "mensagem";
+
+	private static final String TODOS_OS_TITULOS = "todosOsTitulos";
+
+	private static final String REDIRECT_TITULOS = "redirect:/titulos/";
+
+	private static final String EDICAO_TITULO = "EdicaoTitulo";
+
 	@Autowired
 	private Titulos titulos;
 	
@@ -38,8 +53,8 @@ public class TituloController {
 			return CADASTRO_TITULO;
 		}
 		titulos.save(titulo);
-		attributes.addFlashAttribute("mensagem","Titulo Cadastrado com sucesso !" );
-		return "redirect:/titulos/novo";
+		attributes.addFlashAttribute(MENSAGEM,CADASTRO_COM_SUCESSO );
+		return REDIRECT_TITULOS;
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
@@ -51,11 +66,18 @@ public class TituloController {
 	}
 	@RequestMapping("{codigo}")
 	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo) {
-		ModelAndView mv = new ModelAndView(CADASTRO_TITULO);
+		ModelAndView mv = new ModelAndView(EDICAO_TITULO);
 		mv.addObject(titulo);
 		return mv;
 	}
-	@ModelAttribute("todosOsTitulos")
+	
+	@RequestMapping(method = RequestMethod.PUT,path = "/{codigo}")
+	public String editar (@ModelAttribute("codigo")  Titulo titulo,Errors erros,RedirectAttributes attributes ) {
+		titulos.saveAndFlush(titulo);
+		attributes.addFlashAttribute(MENSAGEM,EDITADO_COM_SUCESSO );
+		return REDIRECT_TITULOS;
+	}
+	@ModelAttribute(TODOS_OS_TITULOS)
 	public List<StatusTitulo> todosStatusList(){
 		return Arrays.asList(StatusTitulo.values());
 		
